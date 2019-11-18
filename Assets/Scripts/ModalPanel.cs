4dfb48f170a90f3ab12189eb486af67b4ec9d6cf
U-BSD\323382 <323382@SHS-10DEC4CE4.beaverton.k12.or.us>
2019-11-18 09:14:27 -0800

    Finished pt1 of modal window tutorial

---
+++ Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalPanel : MonoBehaviour {
+
+	public Text question;
+	public Image iconImage;
+	public Button yesButton;
+	public Button noButton;
+	public Button okayButton;
+	public GameObject modalPanelObject;
+	
+	private static ModalPanel modalPanel;
+	
+	public static ModalPanel Instance(){
+		if(!modalPanel){
+			modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+			if(!modalPanel){
+				Debug.LogError("There neds to be one active ModalPanel script on a GameObject in your scene.");
+			}
+		}
+		return modalPanel;
+	}
+	
+	// Yes/No/Cancel: A string, A yes event, a no event, and an okay event
+	public void Choice(string question, UnityAction yesEvent, UnityAction noEvent, UnityAction okayEvent){
+		//open panel
+		modalPanelObject.SetActive(true);
+		
+		//asign listeners
+		yesButton.onClick.RemoveAllListeners();
+		yesButton.onClick.AddListener(yesEvent);
+		yesButton.onClick.AddListener(ClosePanel);
+		
+		noButton.onClick.RemoveAllListeners();
+		noButton.onClick.AddListener(noEvent);
+		noButton.onClick.AddListener(ClosePanel);
+		
+		okayButton.onClick.RemoveAllListeners();
+		okayButton.onClick.AddListener(okayEvent);
+		okayButton.onClick.AddListener(ClosePanel);
+		
+		this.question.text = question;
+		
+		this.iconImage.gameObject.SetActive(false);
+		yesButton.gameObject.SetActive(true);
+		noButton.gameObject.SetActive(true);
+		okayButton.gameObject.SetActive(true);
+	}
+	
+	void ClosePanel(){
+		modalPanelObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/TestModalWindow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TestModalWindow : MonoBehaviour {
+	private ModalPanel modalPanel;
+	private DisplayManager displayManager;
+	
+	private UnityAction myYesAction;
+	private UnityAction myNoAction;
+	private UnityAction myOkayAction;
+	
+	void Awake(){
+		modalPanel = ModalPanel.Instance();
+		displayManager = DisplayManager.Instance();
+		
+		myYesAction = new UnityAction(TestYesFunction);
+		myNoAction = new UnityAction(TestNoFunction);
+		myOkayAction = new UnityAction(TestOkayFunction);
+	}
+	
+	//Send to the Modal Panel to set up the Buttons and Functions to call
+	public void TestYNO(){
+		modalPanel.Choice("Would you like some icecream?\nHow about vanilla?", myYesAction, myNoAction, myOkayAction);
+		
+	}
+	
+	
+	//These are wrapped into UnityActions
+	void TestYesFunction(){
+		displayManager.DisplayMessage("Yes!");
+	}
+	
+	void TestNoFunction(){
+		displayManager.DisplayMessage("No way!");
+	}
+	
+	void TestOkayFunction(){
+		displayManager.DisplayMessage("That's alright");
+	}
+	
+}
